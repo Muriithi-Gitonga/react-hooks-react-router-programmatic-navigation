8dbb7731138c7bc0b144a6569a18b0fb492637ff
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-10-02 21:30:53 +0300

    learnt

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ function Home({ isLoggedIn }) {
       <h1>Home!</h1>
     </div>
   );
+  
 }
 
 export default Home;
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,7 @@ function Login({ setIsLoggedIn }) {
     password: "",
   });
 
+  
   function handleChange(e) {
     setFormData({
       ...formData,
